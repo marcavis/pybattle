9562f1888ecf1dfc63dea6466d920ca3a4e8acd6
Marcos Avila Isidoro <marcavis@yahoo.com.br>
2019-03-05 16:25:23 -0300

    sistema de velocidade

--- Character.py
+++ Character.py
@@ -1,18 +1,44 @@
+import random
 def showStat(stat):
     return '{:02d}'.format(stat[0]) + "/" + '{:02d}'.format(stat[1])
 
+def weaponFactor(entity):
+    return 0.75 + random.random() / 2
 
 class Character:
-    def __init__(self, name, pv, ph, at, df, ag):
+    def __init__(self, name, pv, ph, at, df, ag, control):
         self.name = name
-        self.pv = (pv, pv)
-        self.ph = (ph, ph)
-        self.at = (at, at)
-        self.df = (df, df)
-        self.ag = (ag, ag)
+        self.pv = (pv, pv) #vida
+        self.ph = (ph, ph) #magia
+        self.at = (at, at) #ataque
+        self.df = (df, df) #defesa
+        self.ag = (ag, ag) #agilidade
+        self.control = control #diz se é controlado pelo jogador
+        self.progress = 0 #quão próximo está da prox. rodada
+        self.alive = True
+
+    def attack(self, target):
+        print(self.name + " atacou " + target.name)
+        target.attacked(self)
+
+    def attacked(self, origin):
+        self.damage((origin.at[0] - self.df[0]) * weaponFactor(origin))
+
+    def damage(self, amount):
+        amount = round(amount)
+        if(amount > 0):
+            self.pv = (self.pv[0] - amount, self.pv[1])
+            print(self.name + " sofreu " + str(amount) + " pontos de dano.")
+        if(self.pv[0] < 1):
+            self.alive = False
+
+    def act(self):
+        self.progress -= 1000
+        if(self.control):
+            print("O que você vai fazer?")
 
     def __str__(self):
         return '{:>16}'.format(self.name) + \
         ": PV:" + showStat(self.pv) + ", PH:" + showStat(self.ph) + \
          ", AT:" + showStat(self.at) + ", DF:" + showStat(self.df) + \
-         ", AG:" + showStat(self.ag)
+         ", AG:" + showStat(self.ag) + ", PTR:" + str(self.progress) + "/1000"
--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python3
 from Character import Character
 import random
-players = [Character("Zheng Xiulan", 18, 14, 14, 10, 15),
-        Character("Miao Lin", 22, 10, 16, 11, 18),
-        Character("Tao Jiang", 26, 6, 18, 13, 13),
-        Character("Liu Jingsheng", 20, 8, 14, 10, 14),
-        Character("Jiang Xun", 23, 10, 16, 12, 17),
-        Character("Guan Long", 25, 9, 17, 14, 12)]
-enemies = [Character("Jueyuan", 15, 0, 14, 9, 20),
-        Character("Jueyuan", 15, 0, 14, 9, 20),
-        Character("Jueyuan", 15, 0, 14, 9, 20)]
+def isDead(entities):
+    return [x.pv[0] <= 0 for x in entities]
+
+players = [Character("Zheng Xiulan", 18, 14, 14, 10, 15, True),
+        Character("Miao Lin", 22, 10, 16, 11, 18, True),
+        Character("Tao Jiang", 26, 6, 18, 13, 13, True),
+        Character("Liu Jingsheng", 20, 8, 14, 10, 14, True),
+        Character("Jiang Xun", 23, 10, 16, 12, 17, True),
+        Character("Guan Long", 25, 9, 17, 14, 12, True)]
+enemies = [Character("Jueyuan", 15, 0, 14, 9, 20, False),
+        Character("Jueyuan", 15, 0, 14, 9, 20, False),
+        Character("Jueyuan", 15, 0, 14, 9, 20, False)]
+
+entities = [x for x in players] + [x for x in enemies]
 
 op = "0"
-while(op != "9"):
+battleOver = False
+while(not battleOver):
     print("Enemies:")
     for e in enemies:
         print(e)
     print("\nHeroes:")
     for p in players:
         print(p)
-    op = input("What is your choice? 9 - Quit")
+
+    # op = input("What is your choice? 9 - Quit")
+    # if(all(isDead(enemies))):
+    #     battleOver = True
+
+    for e in entities:
+        e.progress += e.ag[0] + 1
+    for e in entities:
+        if e.progress > 1000:
+            e.act()
